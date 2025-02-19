86e5e5f48bb76d6d465cb4e934848b3ecf97eba1
narsi-jangid <narsijangid01@gmail.com>
2025-02-19 22:23:11 +0530

    sdfg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -6,9 +6,6 @@ import {
   Route
 } from "react-router-dom";
 
-/**
- * ?  =====Import Components=====
- */
 import Home from './Pages/Home';
 import Create from './Pages/Create';
 import ProductPage from './Pages/ProductPage';
@@ -21,36 +18,39 @@ import EditPage from './Pages/EditPage';
 import SellerProfile from './Pages/SellerProfile';
 import EditPostPage from './Pages/EditPostPage';
 
-
-
-
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500); // 1.5 seconds splash duration
+  }, []);
+
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path="/create">
-            <Create />
-          </Route>
-          <Route path="/item/:productId">
-            <ProductPage />
-          </Route>
-          <Route path="/search/:searchId">
-            <SearchPage />
-          </Route>
-          <Route path='/myads' component={MyAdsPage} />
-          <Route path='/myfavorites' component={MyAdsPage} />
-          <Route path='/chat/:chatId' component={ChatPage} />
-          <Route path='/myprofile' component={MyProfilePage} />
-          <Route path='/editprofile/:editInfo' component={EditPage} />
-          <Route path='/profile/:profileId' component={SellerProfile} />
-          <Route path='/editpost/:postId' component={EditPostPage} />
-          <Route component={PageNotFound} path='*' />
-        </Switch>
-      </Router>
+      {loading ? (
+        <div className="splash-screen">
+          <img src="https://imagizer.imageshack.com/img922/3706/Q1vJOp.png" alt="Splash Logo" className="splash-logo" />
+        </div>
+      ) : (
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path="/create" component={Create} />
+            <Route path="/item/:productId" component={ProductPage} />
+            <Route path="/search/:searchId" component={SearchPage} />
+            <Route path='/myads' component={MyAdsPage} />
+            <Route path='/myfavorites' component={MyAdsPage} />
+            <Route path='/chat/:chatId' component={ChatPage} />
+            <Route path='/myprofile' component={MyProfilePage} />
+            <Route path='/editprofile/:editInfo' component={EditPage} />
+            <Route path='/profile/:profileId' component={SellerProfile} />
+            <Route path='/editpost/:postId' component={EditPostPage} />
+            <Route component={PageNotFound} path='*' />
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 }
